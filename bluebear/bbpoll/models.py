1d740ed4246a975b9a958357b87a511ff92f35b8
Robert Chapman <blargony@users.noreply.github.com>
2018-09-15 19:51:52 -0700

    Added a docstring.

--- bluebear/bbpoll/models.py
+++ bluebear/bbpoll/models.py
@@ -6,11 +6,12 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
+        """List of Questions created in the past day."""
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
