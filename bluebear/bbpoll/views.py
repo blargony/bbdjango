f32019915afdee22fc7d33e06421b6043b7a94df
Siria Chapman <schapman@kulikuli.net>
2018-09-29 10:30:29 -0700

    first views

--- bluebear/bbpoll/views.py
+++ bluebear/bbpoll/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Bluebear rocks!")
+    Questions=Question.objects.all()
+    output=""
+    for q in Questions:
+        output += q.question_text
+        output += "\n"
+        
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question {}.".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
