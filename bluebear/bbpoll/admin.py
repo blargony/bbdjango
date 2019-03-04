00853979234b028c057be297d9d992f069159c49
Siria Chapman <schapman@kulikuli.net>
2019-03-03 20:15:46 -0800

    Added Choices as an inline in the admin page

--- bluebear/bbpoll/admin.py
+++ bluebear/bbpoll/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ ChoiceInLine, ]
+
+
+admin.site.register(Question, QuestionAdmin)
